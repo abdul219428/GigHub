bfa844659ff9688e1c820021760a2c38dbf34aa9
Abdul Gaffar Shaikh <abdul219428@gmail.com>
2019-12-13 20:18:45 +0530

    Add project files.

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+   
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+        {
+            public DbSet<Gig> Gigs { get; set; }
+            public DbSet<Genre> Genres { get; set; }
+            public ApplicationDbContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+            {
+            }
+
+            public static ApplicationDbContext Create()
+            {
+                return new ApplicationDbContext();
+            }
+        }
+    
+}
