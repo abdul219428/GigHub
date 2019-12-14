68401c1f3fef3b355fb84b05e7a5aa6659e66249
Abdul Gaffar Shaikh <abdul219428@gmail.com>
2019-12-14 13:22:39 +0530

    Implement validation

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -6,18 +6,27 @@ using System.ComponentModel.DataAnnotations;
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
-    {
+    {   [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime
+
+        public DateTime GetDateTime()
         {
-            get
-            {
+            
                 return DateTime.Parse(string.Format("{0} {1}", Date, Time));
-            }
+            
         }
     }
 }
